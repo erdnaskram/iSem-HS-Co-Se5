ae456df24495413617e2b1a37a7d2a2070800622
Erdna's Kram <72553603+erdnaskram@users.noreply.github.com>
2023-01-06 01:17:27 +0100

    Vue guestbook.html fertig

---
+++ VueHTML/js/app-guestbook.js
@@ -0,0 +1,86 @@
+let gaestebuch;
+
+window.onload
+{
+    getData();
+}
+
+function getData() {
+    let request = new XMLHttpRequest();
+    request.open("GET", 'https://api.mockaroo.com/api/a4f9d370?count=50&key=82ef44b0', false);
+    request.send(null);
+    gaestebuch = JSON.parse(request.responseText);
+}
+
+Vue.createApp({
+    data() {
+        return {
+            gaestebuch: gaestebuch,
+            aktuelleSeitenZahl: 1,
+            suchParameter: '',
+        }
+    },
+    computed: {
+        gefiltertesGaestebuch() {
+            return this.gaestebuch.filter(eintrag => {
+                const vorname = eintrag.name.vorname.toString().toLowerCase();
+                const nachname = eintrag.name.nachname.toString().toLowerCase();
+
+                const searchTerm = this.suchParameter.toLowerCase();
+
+                return vorname.includes(searchTerm) ||
+                    nachname.includes(searchTerm);
+            });
+        },
+        gaestebuchSeiten() {
+            let seiten = [];
+            let seite = [];
+            for (let i = 0; i < this.gefiltertesGaestebuch.length; i++) {
+                if (seite.length < 4)
+                    seite.push(this.gefiltertesGaestebuch[i]);
+                else {
+                    seiten.push(seite);
+                    seite = [];
+                    seite.push(this.gefiltertesGaestebuch[i]);
+                }
+            }
+            if (seite !== [])
+                seiten.push(seite);
+            console.log(seiten);
+            this.$forceUpdate();
+            return seiten;
+        },
+        seitenZahlen() {
+            let zahlen = [];
+            for (let i = 0; i < this.gefiltertesGaestebuch.length; i += 4) {
+                zahlen.push(i / 4 + 1)
+            }
+            return zahlen;
+        },
+        aktuelleSeite() {
+            return this.gaestebuchSeiten[this.aktuelleSeitenZahl - 1];
+        },
+        ersteSeite() {
+            return this.aktuelleSeitenZahl === 1;
+        },
+        letzteSeite() {
+            return this.aktuelleSeitenZahl === this.gaestebuchSeiten.length;
+        }
+    },
+    methods: {
+        wechselSeite(seite) {
+            switch (seite) {
+                case "nächste":
+                    this.aktuelleSeitenZahl++;
+                    break;
+                case "zurück":
+                    this.aktuelleSeitenZahl--;
+                    break;
+                default:
+                    this.aktuelleSeitenZahl = seite;
+                    break;
+            }
+            document.documentElement.scrollTop = 480;
+        }
+    }
+}).mount('.app');
