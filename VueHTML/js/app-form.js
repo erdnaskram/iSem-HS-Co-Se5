10475b43a37b52c871e02f59a870204bfbd7dda1
Erdna's Kram <72553603+erdnaskram@users.noreply.github.com>
2023-01-07 22:13:59 +0100

    - Form Vue -> Eingabevalidierung

--- VueHTML/js/app-form.js
+++ VueHTML/js/app-form.js
@@ -30,6 +30,12 @@ Vue.createApp({
             },
             hobby: "",
             confirmed: false,
+            mitteilung: false,
+            fehler: false,
+            unvollstaendig: false,
+            falscheDaten: false,
+            erfolg: false,
+            meldung:"",
         }
     },
     computed: {},
@@ -49,7 +55,65 @@ Vue.createApp({
         },
         confirmForm() {
             this.gaestebuch.push(this.neuerEintrag);
-            this.confirmed = true;
+            if (this.pruefeInput()) {
+                this.erfolg = true;
+                this.meldung = "Dein Gästebucheintrag wurde erfolgreich erstellt!"
+                this.confirmed = true;
+            } else {
+                this.fehler = true;
+                if (this.unvollstaendig)
+                    this.meldung = "Eingaben unvollständig! Bitte alle Felder Füllen"
+                else if (this.falscheDaten)
+                    this.meldung = "Eingaben Fehlerhaft! Bitte alle Felder überprüfen"
+            }
+            this.mitteilung=true;
+        },
+        pruefeInput(){
+            this.fehler = false;
+            this.unvollstaendig = false;
+            this.falscheDaten = false;
+            this.erfolg = false;
+            let inputVollstaendig = true;
+
+            if (this.neuerEintrag.name.vorname === "" ||
+                    this.neuerEintrag.name.nachname === "" ||
+                    this.neuerEintrag.geburtstag === "" ||
+                    this.neuerEintrag.mail === "" ||
+                    this.neuerEintrag.plz === "" ||
+                    this.neuerEintrag.wohnort === "" ||
+                    this.neuerEintrag.hobbys.length === 0 ||
+                    this.neuerEintrag.nachricht === ""){
+                inputVollstaendig = false;
+                this.unvollstaendig = true;
+            }
+
+
+            if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(this.neuerEintrag.mail)){
+                inputVollstaendig = false;
+                this.falscheDaten = true;
+            }
+
+            if (!/^[0-9]{5}$/.test(this.neuerEintrag.plz)){
+                inputVollstaendig = false;
+                this.falscheDaten = true;
+            }
+
+
+            if (this.neuerEintrag.geburtstag > "2023-01-01" ||
+                this.neuerEintrag.geburtstag < "1920-01-01"){
+                inputVollstaendig = false;
+                this.falscheDaten = true;
+            }
+
+
+
+
+            return inputVollstaendig;
+        },
+        schließeMeldung(){
+            this.fehler = false;
+            this.erfolg = false;
+            this.mitteilung = false;
         }
     }
 }).mount('.app');
